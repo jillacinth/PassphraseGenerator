d70d095cf6499e6af554e3a663dee2ea56957af4
jillacinth <jill30jack@gmail.com>
2025-02-04 18:39:05 -0500

    Setup with Node.js and Next

---
+++ components/ExampleComponent.js
@@ -0,0 +1,6 @@
+// components/ExampleComponent.js
+import React from 'react';
+
+export const ExampleComponent = () => {
+  return <div>This is an example component</div>;
+};
---
+++ pages/index.js
@@ -0,0 +1,15 @@
+// pages/index.js
+import React from 'react';
+import { ExampleComponent } from '../components/ExampleComponent';
+
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Welcome to Next.js and React</h1>
+      <ExampleComponent />
+    </div>
+  )
+}
+
+export default Home
